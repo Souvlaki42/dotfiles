7744f289796ab1c8338f223d15f42cc0ec275d67
Souvlaki42 <ilias@moulas.dev>
2026-04-26 23:35:03 +0300

    fix: backend nvim changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,15 +1,15 @@
 kset = vim.keymap.set
 
 vim.filetype.add({
-  pattern = {},
-  filename = {},
-  content = function(_, content)
-    local first_line = content[1]
-    if first_line and first_line:match("^#!.*bash") then
-      return "sh"
-    end
-    return nil
-  end,
+	pattern = {},
+	filename = {},
+	content = function(_, content)
+		local first_line = content[1]
+		if first_line and first_line:match("^#!.*bash") then
+			return "sh"
+		end
+		return nil
+	end,
 })
 
 require("core.options")
