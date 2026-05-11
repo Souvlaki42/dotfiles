36fed283e19acdbe9f25ffb85199f8b4fe3b9e7c
Souvlaki42 <ilias@moulas.dev>
2026-05-12 02:02:56 +0300

    feat: new capabilities api

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -68,12 +68,14 @@ return {
 
 		-- Add cmp_nvim_lsp capabilities settings to lspconfig
 		-- This should be executed before you configure any language server
-		local lspconfig_defaults = require("lspconfig").util.default_config
-		lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-			"force",
-			lspconfig_defaults.capabilities,
-			require("cmp_nvim_lsp").default_capabilities()
-		)
+		local default_capabilities = vim.lsp.protocol.make_client_capabilities()
+		vim.lsp.config("*", {
+			capabilities = vim.tbl_deep_extend(
+				"force",
+				default_capabilities,
+				require("cmp_nvim_lsp").default_capabilities()
+			),
+		})
 
 		-- This is where you enable features that only work
 		-- if there is a language server active in the file
