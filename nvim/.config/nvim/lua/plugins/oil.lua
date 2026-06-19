fe8047a78fa329303dd90e6ee751e2ac9902bb2c
Souvlaki42 <ilias@moulas.dev>
2026-06-19 22:03:06 +0300

    feat: added oil nvim winbar with abs path, watch for changes and keybind for executing command under current entry

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -4,6 +4,17 @@ return {
 	config = function()
 		local oil = require("oil")
 
+		function _G.get_oil_winbar()
+			local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+			local dir = require("oil").get_current_dir(bufnr)
+			if dir then
+				return vim.fn.fnamemodify(dir, ":~")
+			else
+				-- If there is no current directory (e.g. over ssh), just show the buffer name
+				return vim.api.nvim_buf_get_name(0)
+			end
+		end
+
 		oil.setup({
 			default_file_explorer = true,
 			delete_to_trash = true,
@@ -22,7 +33,36 @@ return {
 				end,
 			},
 			win_options = {
+				winbar = "%!v:lua.get_oil_winbar()",
 				wrap = true,
+			},
+			watch_for_changes = true,
+			keymaps = {
+				["gc"] = {
+					callback = function()
+						local entry = oil.get_cursor_entry()
+						if entry == nil then
+							print("No entry is here")
+							return
+						end
+
+						local name = entry.parsed_name
+						if name == nil then
+							print("No entry name")
+							return
+						end
+
+						vim.ui.input({ prompt = "Give command: " }, function(input)
+							if input == nil or string.gsub(input, " ", "") == "" then
+								return
+							end
+							local command = string.gsub(input, "%%", name)
+							print(vim.fn.system(command))
+						end)
+					end,
+					mode = "n",
+					desc = "Execute a shell command on the selected entry",
+				},
 			},
 		})
 
